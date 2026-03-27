ce33a9663433946f4f241d0a442af022877faaa8
Aleksey Nikolaev <faithinlove5.an@gmail.com>
2026-03-27 18:43:44 +0300

    font

--- js/formMap.js
+++ js/formMap.js
@@ -1,16 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const dealerSelect = document.getElementById("dealerSelect");
   const mapBlock = document.querySelector(".map");
-  const formReviw = document.querySelector('.form-reviw');
 
   function toggleMap() {
     if (dealerSelect.value !== "") {
       mapBlock.style.display = "block";
-      formReviw.style.marginLeft = "40px";
-
     } else {
       mapBlock.style.display = "none";
-      formReviw.style.marginLeft = "0px";
     }
   }
 
